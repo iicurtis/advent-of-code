77e288c63be84756ea010757c77cf7c3c822b781
Isaac Curtis <iicurtis@outlook.com>
2021-12-01 07:59:21 -0700

    2021 day01 shorter

--- 2021/rust/src/day01.rs
+++ 2021/rust/src/day01.rs
@@ -3,42 +3,33 @@
 type Error = Box<dyn std::error::Error>;
 
 pub fn solve(input: &str) -> Result<String, Error> {
-    let soln1 = part1(input);
-    let soln2 = part2(input);
+    let input = parse(input);
+    let soln1 = part1(&input);
+    let soln2 = part2(&input);
     Ok(format!("Part 1: {}\nPart 2: {}", soln1, soln2))
 }
 
-pub fn part1(input: &str) -> usize {
-    let input: Vec<usize> = input
+pub fn parse(input: &str) -> Vec<usize> {
+    input
         .trim()
         .lines()
         .map(|line| line.parse().unwrap())
-        .collect();
-    let mut increased_count = 0;
-    let mut prev_depth = usize::max_value();
-    for depth_measurement in input {
-        if depth_measurement > prev_depth {
-            increased_count += 1;
-        }
-        prev_depth = depth_measurement;
-    }
-    increased_count
+        .collect()
 }
 
-pub fn part2(input: &str) -> usize {
-    let input: Vec<usize> = input
-        .trim()
-        .lines()
-        .map(|line| line.parse().unwrap())
-        .collect();
+pub fn part1(input: &[usize]) -> usize {
+    // its pretty but 5% slower than the loop version
+    input.windows(2).filter(|pair| pair[0] < pair[1]).count()
+}
+
+pub fn part2(input: &[usize]) -> usize {
     let mut increased_count = 0;
     let mut prev_sum = usize::max_value();
     for i in 0..input.len() - 2 {
-        let window_sum = input[i] + input[i + 1] + input[i + 2];
-        if window_sum > prev_sum {
+        if input[i + 2] > prev_sum {
             increased_count += 1;
         }
-        prev_sum = window_sum;
+        prev_sum = input[i];
     }
     increased_count
 }
@@ -61,7 +52,7 @@ mod test {
 260
 263
 "#;
-        assert_eq!(part1(&input), 7);
+        assert_eq!(part1(&parse(input)), 7);
     }
 
     #[test]
@@ -78,6 +69,6 @@ mod test {
 260
 263
 "#;
-        assert_eq!(part2(&input), 5);
+        assert_eq!(part2(&parse(input)), 5);
     }
 }
