d2efa7edb7273d29e10fb3cb85d8b7e37742f7c1
Isaac Curtis <iicurtis@outlook.com>
2023-09-22 12:54:10 -0600

    julia day 04

--- 2022/julia/src/day01.jl
+++ 2022/julia/src/day01.jl
@@ -2,13 +2,13 @@ module Day01
 
 using AdventOfCode2022
 
-function day01(input::String = joinpath(@__DIR__, "..", "..", "inputs", "day01.txt"))
-    input = open(input, "r") do file
-        read(file, String)
-    end
-    cal_sum = map(x -> parse.(Int, eachsplit(x)) |> sum, eachsplit(input, "\n\n"))
-    partialsort!(cal_sum, 1:3, rev=true)
-    return [cal_sum[1], cal_sum[1:3] |> sum]
+function day01(input::String=joinpath(@__DIR__, "..", "..", "inputs", "day01.txt"))
+  input = open(input, "r") do file
+    read(file, String)
+  end
+  cal_sum = map(x -> parse.(Int, eachsplit(x)) |> sum, eachsplit(input, "\n\n"))
+  partialsort!(cal_sum, 1:3, rev=true)
+  return [cal_sum[1], cal_sum[1:3] |> sum]
 end
 
 end # module
